fc40422dc55c2cd8c63b62e7c5d87fcfe5b53870
tinaparija <tinaparija@gmail.com>
2018-02-28 11:48:24 -0800

    seed updates

--- seed.js
+++ seed.js
@@ -14,21 +14,31 @@ var usersList = [
 ];
 
 var clipsList = [{
-  concept: 'life change',
+  concept: 'new concept',
   content: 'A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.',
   date: new Date('January 21, 2018')
 },{
-  concept: 'new topic',
+  concept: 'new concept',
   content: 'A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.',
-  date: new Date('February 22, 2018')
+  date: new Date('January 28, 2018')
 },{
-  concept: 'new topic',
+  concept: 'new concept',
   content: 'A paragraph (from the Acient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.',
-  date: new Date('February 23, 2018 ')
+  date: new Date('February 5, 2018 ')
 },{
-  concept: 'new topic',
+  concept: 'new concept',
   content: 'A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.',
-  date: new Date('March 24, 2018')
+  date: new Date('February 14, 2018')
+},
+{
+  concept: 'new concept',
+  content: 'A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.',
+  date: new Date('February 14, 2018')
+},
+{
+  concept: 'new concept',
+  content: 'A paragraph (from the Ancient Greek παράγραφος paragraphos, "to write beside" or "written beside") is a self-contained unit of a discourse in writing dealing with a particular point or idea. Though not required by the syntax of any language, paragraphs are usually an expected part of formal writing, used to organize longer prose.',
+  date: new Date('February 28, 2018')
 },
 
 ]
